29fd8f7b6c9a313d17dea97fb682a5f96df55657
Anni Lange <maocypher@gmail.com>
2020-11-16 17:48:21 +0100

    Connection test view

--- octoprint_smart_filament_sensor/static/js/smartfilamentsensor_settings.js
+++ octoprint_smart_filament_sensor/static/js/smartfilamentsensor_settings.js
@@ -3,6 +3,66 @@ $(function(){
         var self = this;
 
         self.settingsViewModel = parameters[0];
+        self.connectionTestDialog = undefined;
+
+        self.remainingDistance = ko.observable(undefined);
+        self.lastMotionDetected = ko.observable(undefined);
+        self.isFilamentMoving = ko.observable(undefined);
+
+        self.onStartup = function() {
+            self.connectionTestDialog = $("#settings_plugin_smartfilamentsensor_connectiontest");
+        };
+        
+        self.showConnectionTest = function() {
+            self.connectionTestDialog.modal({
+                minHeight: function() { return Math.max($.fn.modal.defaults.maxHeight() - 80, 250); },
+                show: true
+            });
+        };
+
+        self.onDataUpdaterPluginMessage = function(plugin, data){
+            if(plugin !== "smartfilamentsensor"){
+                return;
+            }
+            
+            var message = JSON.parse(data);
+            self.remainingDistance(message["_remaining_distance"]);
+            self.lastMotionDetected(message["_last_motion_detected"]);
+            self.isFilamentMoving(message["_filament_moving"]);
+
+            /*if(message["_filament_moving"] == true){
+                self.isFilamentMoving("Yes");
+            }
+            else{
+                self.isFilamentMoving("No");
+            }*/
+        };
+
+        self.startConnectionTest = function(){
+            $.ajax({
+                url: API_BASEURL + "plugin/smartfilamentsensor",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({ "command": "startConnectionTest" }),
+                contentType: "application/json",
+                success: self.RestSuccess
+            });
+        };
+
+        self.stopConnectionTest = function(){
+            $.ajax({
+                url: API_BASEURL + "plugin/smartfilamentsensor",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({ "command": "stopConnectionTest" }),
+                contentType: "application/json",
+                success: self.RestSuccess
+            });
+        };
+
+        self.RestSuccess = function(response){
+            return;
+        }
     }
 
     OCTOPRINT_VIEWMODELS.push({
